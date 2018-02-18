e40dfaac03d7155221bf0269cbf76639593b9cfe
DrewGregory <djgregny@gmail.com>
2018-02-18 04:50:48 -0800

    MAJOR IMPROVEMENTS

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
        temperature: 40,
        selCity: "Los Angeles",
        area: 2000,
-    humanSrc: "happyhuman.svg"};
+    humanSrc: "sadhuman.svg"};
     let self = this;
     httpGetAsync("https://tree-hacks.herokuapp.com/api/weather/" + self.state.selCity, function(data){
       self.setState(prevState =>({
@@ -44,42 +44,69 @@ class App extends Component {
     this.adjustTemperature = this.adjustTemperature.bind(this);
       this.toggleJacket = this.toggleJacket.bind(this);
       this.asyncGraph = this.asyncGraph.bind(this);
+      this.changeArea = this.changeArea.bind(this);
      }
 
+    requestCost(e){
+      var xmlHttp = new XMLHttpRequest();
+      var self = this;
+      xmlHttp.onreadystatechange = function() {
+          if (xmlHttp.readyState === 4 && xmlHttp.status === 200){
+            console.log(JSON.parse(xmlHttp.responseText));
+            self.setState(prevState =>({
+              cost: parseFloat(JSON.parse(xmlHttp.responseText).cost * 15).toFixed(2),
+              energy: parseFloat(JSON.parse(xmlHttp.responseText).energy * 15).toFixed(2)
+            }));
+          }
+      }
+      let reqURL = 'https://tree-hacks.herokuapp.com/api/energycost/' + this.state.selCity + '/'
+      + this.state.temperature + '/' + this.state.area;
+      xmlHttp.open("GET", reqURL,true);
+      xmlHttp.send(null);
+
+    }
 
 
   adjustTemperature(e) {
     e.persist();
-    var newHumanSrc = "happyhuman.svg";
-    let temp = parseInt((660 - e.screenY + 120)/(660-120)*40 + 40);
-    console.log("temperature being adjusted" + e.screenY);
-    if (temp < 68) {
-      newHumanSrc = "sadhuman.svg";
-    }
+    console.log(e);
+    var newHumanSrc = this.state.humanSrc.includes("jacket") ? "humanwithjacket.png" : "human.svg";
+    let temp = parseInt((620 - e.pageY )/(620-212)*40 + 40);
+    newHumanSrc = this.checkIfSad(newHumanSrc, temp);
     this.setState(prevState => ({
-      sliderTop: e.screenY - 50 + "px",
-      height: 660 - e.screenY + 120 + "px",
+      sliderTop: e.pageY + "px",
+      height: 620 - e.pageY + 120 + "px",
       temperature: temp,
       humanSrc: newHumanSrc
       }));
+      this.requestCost();
+
     }
 
+    checkIfSad(src, temp) {
+      let jacket= src.includes("jacket");
+      if (jacket && temp > 75 || !jacket && temp < 65) {
+        src = "sad" + src;
+      }
+      return src;
+    }
 
   toggleJacket(e){
     e.persist();
-    var newHumanSrc = "humanwithjacket.svg";
-    if (this.state.humanSrc == "humanwithjacket.svg") {
-      if (this.state.temperature < 68) {
-        newHumanSrc = "sadhuman.svg";
-      } else {
-        newHumanSrc = "happyhuman.svg";
-     }
-    }
+    var newHumanSrc = this.state.humanSrc.includes("jacket") ? "human.svg" : "humanwithjacket.png";
+    newHumanSrc = this.checkIfSad(newHumanSrc, this.state.temperature);
     this.setState(prevState => ({
-      humanSrc: newHumanSrc
+      humanSrc: newHumanSrc,
     }));
   }
 
+  changeArea (e){
+    e.persist();
+    this.setState(prevState => ({
+      area: parseInt(e.target.value)
+    }));
+    this.requestCost();
+  }
   //This asynchronous rendering is incredibly stupid. There has to be a better way
   asyncGraph(e) {
     e.persist();
@@ -100,40 +127,46 @@ class App extends Component {
     //while(data===null){};
     return (
        <div className="App">
-       <header className="App-header">
-       <h1 className="App-title">Temperament</h1>
-       <img src="thermo_icon.svg" className="App-logo" alt="logo" />
+        <header className="App-header">
+          <h1 className="App-title">Temperament</h1>
+          <p id="subtitle">The Hottest Heating Bill Estimator</p>
+          <img src="thermo_icon.svg" className="App-logo" alt="logo" />
         </header>
-         <p className="App-intro">
+        <p className="App-intro">
          Welcome to Temperament! Input your thermostat settings, square footage, and local area. We will
          estimate your energy usage and cost.
-         </p>
-         	<div>
-         		<img id="thermo"  src="Thermometer.svg" alt="Thermometer" onClick={this.adjustTemperature}/>
-         		<div id="temperature_slider" style={{ top: this.state.sliderTop,
+        </p>
+        <div>
+         	<img id="thermo"  src="Thermometer.svg" alt="Thermometer" onClick={this.adjustTemperature}/>
+         	<div id="temperature_slider" style={{ top: this.state.sliderTop,
              height: this.state.height}} onClick={this.adjustTemperature}>
-         		</div>
-         		<p id="temp_label" style={{top: this.state.sliderTop}}>{this.state.temperature}°F</p>
          	</div>
-          <div id="graph">
+         	<p id="temp_label" style={{top: this.state.sliderTop}}>{this.state.temperature}°F</p>
+        </div>
+        <div id="graph">
             <span id="graph_label"> Outside Temperatures in {this.state.selCity}</span>
             {this.state && this.state.data && new Graph(800,400, this.state.data, "#abcdef").graph()}
-          </div>
-         		<div className="city-text">
-         				<label className="city-text-input col-form-label">City </label>
-         				<input type="text" value="Los Angeles" value={this.state.selCity} onChange={this.asyncGraph} className="city-text-input" id="city-text-field" />
-         		</div>
-             <label className="area-text-input col-form-label">Area of House (ft^2)</label>
-         	<input type="number" value={this.state.area} className="area-text-input" id="area-text-field" />
-          <div id="human">
-          <img src={this.state.humanSrc} onClick={this.toggleJacket} alt="Human"/>
-          </div>
-          <div>
-          Estimated Cost: {this.state.cost}
-          </div>
-          <span id="credits">
-          Developed by Ryan Tolsma, Daniel Tan, Drew Gregory, and Griffin Kardos. Icon Credits go to Luboš Volkov.
-          </span>
+        </div>
+        <div className="city-text">
+          <label className="text-input">City </label>
+          <input type="text" value="Los Angeles" value={this.state.selCity} onChange={this.asyncGraph} className="text-input" id="city-text-field" />
+        </div>
+        <div className="form-group">
+          <label className="text-input">Area of House (ft^2)</label>
+          <input type="number" value={this.state.area} className="text-input form-text" id="area-text-field" onChange={this.changeArea} />
+        </div>
+        <div >
+          <img id="human" src={this.state.humanSrc} onClick={this.toggleJacket} alt="Human"/>
+        </div>
+        <div id="est_cost" className="badge">
+          Estimated Cost: $ {this.state.cost}/month
+        </div>
+        <div id="est_energy" className="badge">
+          Estimated Energy: {this.state.energy} kWH/month
+        </div>
+        <span id="credits">
+          Developed by Ryan Tolsma, Daniel Tan, Drew Gregory, and Griffin Kardos. Icon Credits go to Luboš Volkov. Estimated worse case cost.
+        </span>
        </div>
     );
   }
